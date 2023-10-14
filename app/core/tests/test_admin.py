d921f0db270e9ab41f14524a8d7498aa670e2741
jvpigozzo <jvpigozzo@gmail.com>
2023-10-14 15:43:08 -0300

    add test for user

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -32,3 +32,10 @@ class AdminSiteTests(TestCase):
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add',args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
