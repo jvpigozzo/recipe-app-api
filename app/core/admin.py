726af059b8784e7c159a8e2b37d8328897d1602f
jvpigozzo <jvpigozzo@gmail.com>
2023-10-14 16:16:36 -0300

    update user admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -6,7 +6,6 @@ from core import models
 
 
 class UserAdmin(BaseUserAdmin):
-    """Define admin pages for users"""
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
@@ -14,9 +13,19 @@ class UserAdmin(BaseUserAdmin):
         (_('Personal Info'), {'fields': ('name',)}),
         (
             _('Permissions'),
-            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+            {'fields': ('is_active',
+                        'is_staff',
+                        'is_superuser')}
         ),
         (_('Important dates'), {'fields': ('last_login',)})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email',
+                    'password1',
+                    'password2')
+        }),
     )
 
 admin.site.register(models.User, UserAdmin)
